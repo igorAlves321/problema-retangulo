79dd62daf9b427896a3738330155a1c23ec77e30
igorAlves321 <igoralvespalmeirag4@gmail.com>
2024-03-14 04:54:51 -0300

    feita a primeira classe, e adcionado o métudo para calcular a área.

---
+++ Retangulo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ProblemaRetangulo
+{
+    public class Retangulo
+    {
+         public double altura;
+        public double largura;
+
+        public double calcularArea()
+        {
+            double area = altura * largura;
+            return area;
+        }
+    }
+}
